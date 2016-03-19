51049f3a620b2fe5ae347c7375a2f847ffdf67da
UltimateByte <robin.labadie@gmail.com>
2016-03-19 03:38:23 +0100

    missing space

--- lgsm/gameservermanagers.sh
+++ lgsm/gameservermanagers.sh
@@ -91,7 +91,7 @@ fn_fetch_core_dl
 }
 
 fn_lgsm_config(){
-if [ ! -f "${rootdir}/${selfname}.cfg"]; then
+if [ ! -f "${rootdir}/${selfname}.cfg" ]; then
 	echo "Downloading configuration file..."
 	wget --no-cache "https://raw.githubusercontent.com/${githubuser}/${githubrepo}/${githubbranch}/config/lgsm.cfg" -O "${lgsmconf}" 2>&1
 	chmod +x "${lgsmconf}"
